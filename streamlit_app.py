be0eef0ca5168a4ddadd933fc4db87acc7c293fd
LucasBadge2 <148252561+LucasBadge2@users.noreply.github.com>
2023-10-23 13:39:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,9 +42,3 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.title("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-
-add_myfruit=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-fruits_added_to_show = my_fruit_list.loc[add_myfruit]
-
-# Display the table on the page.
-streamlit.dataframe(fruits_added_to_show)
