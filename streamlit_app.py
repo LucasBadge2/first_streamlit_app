3a0932a2258b9316f92d6754001dfb43d3c3b26d
LucasBadge2 <148252561+LucasBadge2@users.noreply.github.com>
2023-10-24 12:02:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import UrlError
+from urllib.error import URLError
 
 streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
